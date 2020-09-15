b2b49d74d5a3c246bd3fa5aaaeedcc4fc64dd305
Seth Harris <sharris@designsensory.com>
2020-09-15 09:19:09 -0400

    change manifest in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `seth-harris-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#141414`,
+        theme_color: `#141414`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sh_logo_final.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
